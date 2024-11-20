02633621c0c10ff55b6b79268ae1927e7433cb6c
LeoSebastian23 <leosebastiang19@gmail.com>
2024-11-20 18:57:47 -0300

    add lombok

--- src/main/java/com/example/api_adopciones/Models/TipoMascota.java
+++ src/main/java/com/example/api_adopciones/Models/TipoMascota.java
@@ -1,28 +1,19 @@
 package com.example.api_adopciones.Models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tipo_mascota")
+@Getter
+@Setter
 public class TipoMascota {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "tipo", length = 50, nullable = false)
     private String tipo;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
 }
+
